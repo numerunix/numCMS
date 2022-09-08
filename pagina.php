475547753199472e24a39c111ff81fe27bc9d25e
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-09-08 17:59:09 +0200

    Add files via upload

---
+++ pagina.php
@@ -0,0 +1,10 @@
+<?php
+$contenuto='<h1>CComixManager</h1>
+<p>Sorgenti di <a href="Downloads/ccomixmanager-0.1.2.6.tar.xz">CComixManager 0.1.2.6</a><br />
+<a href="https://github.com/numerunix/ccomixmanager">Home page ufficiale del progettio</a><br />
+I binari si possono scaricare dalla pagina ufficiale del progetto.<br />
+<a href="https://snapcraft.io/ccomixmanager">
+  <img alt="Scarica dallo Snap Store" src="https://snapcraft.io/static/images/badges/it/snap-store-black.svg" />
+</a>
+</p>';
+?>
